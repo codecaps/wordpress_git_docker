1e2a8b560e2ab238601ac645de45b733f07aa795
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2026-06-24 11:17:44 +0200

    lower workers test

--- scripts/configure_wordpress_fpm_conf.sh
+++ scripts/configure_wordpress_fpm_conf.sh
@@ -23,11 +23,11 @@ listen.backlog = 2048
 ; Example:
 ;   768MB available / 64MB per worker ≈ 12 workers
 pm = dynamic
-pm.max_children = 120
-pm.start_servers = 30
-pm.min_spare_servers = 30
-pm.max_spare_servers = 60
-pm.max_requests = 5000
+pm.max_children = 24
+pm.start_servers = 6
+pm.min_spare_servers = 6
+pm.max_spare_servers = 12
+pm.max_requests = 1000
 
 ; Kill very slow PHP requests instead of letting workers hang forever.
 request_terminate_timeout = 120s
